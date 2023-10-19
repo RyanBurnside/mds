340b05b66fa61f2c2cbe268e158616e5fbbb7659
Ryan Burnside <pixeloutlaw@gmail.com>
2023-10-18 22:16:44 -0600

    Cleaned up some source code.

--- levels.lisp
+++ levels.lisp
@@ -8,12 +8,12 @@
 
 (defun make-emitter (x y)
   ;; Generic function to make a simple version of our emitter
-  (make-instance 'emitter 
-		 :repeating t
-		 :parent-x x
-		 :parent-y y
-		 :aim-player-func #'direction-to-player
-		 :shot-push-func #'shot-adapter-function))
+  (make-instance 'emitter
+                 :repeating t
+                 :parent-x x
+                 :parent-y y
+                 :aim-player-func #'direction-to-player
+                 :shot-push-func #'shot-adapter-function))
 
 ;; Level 1 (single emitter 7 arms swirley :) )
 (defun level-1 ()
@@ -22,54 +22,54 @@
 
     (dotimes (i 100)
       (let ((percent (/ i 99.0)))
-	(push-burst a 
-		    :direction (* i percent)
-		    :spread (* TAU 6/7) 
-		    :speed 4 
-		    :num-shots 7
-		    :color (pick *GREEN* *BLUE*)
-		    :step 5)))
-    
+        (push-burst a
+                    :direction (* i percent)
+                    :spread (* TAU 6/7)
+                    :speed 4
+                    :num-shots 7
+                    :color (pick *GREEN* *BLUE*)
+                    :step 5)))
+
     (dotimes (i 10)
-      (push-burst a 
-		  :direction 'player
-		  :spread TAU
-		  :speed 5
-		  :num-shots (+ i 10)
-		  :color (if (oddp i) *YELLOW* *PURPLE*)
-		  :step 3))
+      (push-burst a
+                  :direction 'player
+                  :spread TAU
+                  :speed 5
+                  :num-shots (+ i 10)
+                  :color (if (oddp i) *YELLOW* *PURPLE*)
+                  :step 3))
     (vector-push-extend a *enemies*)))
 
 (defun level-2 ()
   (setf (ready-at *scrape-ticker*) 70)
   (let ((a (make-emitter (* *width* .20) (* *height* .75)))
-	(b (make-emitter (* *width* .80) (* *height* .75)))
-	(c (make-emitter (half-width) (* *height* .75))))
+        (b (make-emitter (* *width* .80) (* *height* .75)))
+        (c (make-emitter (half-width) (* *height* .75))))
 
     (push-burst a :direction 'player :num-shots 4
-		:color *RED* :step 5 :spread (* PI .45)
-		:speed 10)
-    
+                  :color *RED* :step 5 :spread (* PI .45)
+                  :speed 10)
+
     (push-burst b :direction 'player :num-shots 4
-		:color *BLUE* :step 5 :spread (* PI .45)
-		:speed 10)
+                  :color *BLUE* :step 5 :spread (* PI .45)
+                  :speed 10)
 
     (push-burst c :num-shots 1
-		:color *PURPLE*
-		:direction 'player
-		:step 50
-		:speed 1)
+                  :color *PURPLE*
+                  :direction 'player
+                  :step 50
+                  :speed 1)
 
     (dotimes (i 100)
       (let ((dir (random TAU)))
-	(dotimes (j 4)
-	  (push-burst c :direction dir
-		      :color (pick *ORANGE* *YELLOW* *BLACK*)
-		      :spread (* TAU 6/7)
-		      :step 3
-		      :num-shots 6
-		      :speed 6))))
-    
+        (dotimes (j 4)
+          (push-burst c :direction dir
+                        :color (pick *ORANGE* *YELLOW* *BLACK*)
+                        :spread (* TAU 6/7)
+                        :step 3
+                        :num-shots 6
+                        :speed 6))))
+
     (vector-push-extend a *enemies*)
     (vector-push-extend b *enemies*)
     (vector-push-extend c *enemies*)))
@@ -77,34 +77,34 @@
 (defun level-3 ()
   (setf (ready-at *scrape-ticker*) 50)
   (let ((a (make-emitter (* *width* .25) (* *height* .95)))
-	(b (make-emitter (* *width* .75) (* *height* .95)))
-	(sniper (make-emitter (half-width) (* *height* .80))))
+        (b (make-emitter (* *width* .75) (* *height* .95)))
+        (sniper (make-emitter (half-width) (* *height* .80))))
 
     (dotimes (line-count 100)
-      (push-burst a 
-		  :direction (random TAU)
-		  :spread (* TAU 7/8)
-		  :speed 5
-		  :num-shots 7
-		  :color (pick *BLUE* *TEAL*)
-		  :step 6)
-      
+      (push-burst a
+                  :direction (random TAU)
+                  :spread (* TAU 7/8)
+                  :speed 5
+                  :num-shots 7
+                  :color (pick *BLUE* *TEAL*)
+                  :step 6)
+
       (push-burst b
-		  :direction (random TAU)
-		  :spread (* TAU 7/8)
-		  :speed 5
-		  :num-shots 7
-		  :color (pick *YELLOW* *PURPLE*)
-		  :step 6))
-    
+                  :direction (random TAU)
+                  :spread (* TAU 7/8)
+                  :speed 5
+                  :num-shots 7
+                  :color (pick *YELLOW* *PURPLE*)
+                  :step 6))
+
     (push-burst sniper
-		:direction 'player
-		:spread 1
-		:speed 7
-		:num-shots 1
-		:color *RED*
-		:step 100)
-    
+                :direction 'player
+                :spread 1
+                :speed 7
+                :num-shots 1
+                :color *RED*
+                :step 100)
+
     (vector-push-extend sniper *enemies*)
     (vector-push-extend a *enemies*)
     (vector-push-extend b *enemies*)))
--- mds.lisp
+++ mds.lisp
@@ -31,11 +31,11 @@
 
 (defun make-player ()
   (setf *player* (make-instance 'obj
-				:hp 3 ;lives in this case
-				:dead nil
-				:color (vec4 0 0 0 1)
-				:radius .5
-				:pos (vec2 (* *width* .75) (* *height* .20)))))
+                                :hp 3 ;lives in this case
+                                :dead nil
+                                :color (vec4 0 0 0 1)
+                                :radius .5
+                                :pos (vec2 (* *width* .75) (* *height* .20)))))
 
 (defun make-game-container ()
   (make-array 1 :fill-pointer 0 :adjustable t))
@@ -46,23 +46,19 @@
 
 (defun reposition-player ()
   (setf (pos *player*)
-	(vec2 (* *width* .5) (* *height* .20))))
+        (vec2 (* *width* .5) (* *height* .20))))
 
 (defun contain-player ()
-  (when (< (x (pos *player*)) 1)
-    (setf (x (pos *player*)) 1))
-  (when (< (y (pos *player*)) 1)
-    (setf (y (pos *player*)) 1))
-  (when (> (x (pos *player*)) (1- *width*))
-    (setf (x (pos *player*)) (1- *width*)))
-  (when (> (y (pos *player*)) (1- *height*))
-    (setf (y (pos *player*)) (1- *height*))))
+  (with-accessors ((x x) (y y))
+      (pos *player*)
+    (setf x (alexandria:clamp x 1 (1- *width*)))
+    (setf y (alexandria:clamp y 1 (1- *height*)))))
 
 (defun advance-level-if-done ()
   (when (readyp *scrape-ticker*)
     (if (< *level* (length *level-functions*))
-	(incf *level*)
-	(setf *level* 1))
+        (incf *level*)
+        (setf *level* 1))
     (setf *enemy-shots* (make-game-container))
     (setf *enemies* (make-game-container))
     (reposition-player)
@@ -80,47 +76,43 @@
   (setf *lives* 5)
   (setf *score* 0)
   (resetf *scrape-ticker*)
-  (reposition-player)
   (setf *level* 1))
 
 (gamekit:defgame example () ()
-		 (:viewport-width *width*)
-		 (:viewport-height *height*)
-		 (:viewport-title "Minimal Danmaku Simulator"))
+  (:viewport-width *width*)
+  (:viewport-height *height*)
+  (:viewport-title "Minimal Danmaku Simulator"))
 
 (defvar *key-bag* nil)
 
 (defun update-pos ()
-  (let ((move-speed 4))
-    (when (or (member :z *key-bag*)
-	      (member :y *key-bag*)) ;For the Germans :)
-      (setf move-speed 2.0))
-    (when (or (member :left *key-bag*) (member :a *key-bag*))
+  (let ((move-speed (if (intersection '(:z :y) *key-bag*) 2.0 4.0)))
+    (when (intersection '(:left :a) *key-bag*)
       (decf (x (pos *player*)) move-speed))
-    (when (or (member :right *key-bag*) (member :d *key-bag*))
+    (when (intersection '(:right :d) *key-bag*)
       (incf (x (pos *player*)) move-speed))
-    (when (or (member :up *key-bag*) (member :w *key-bag*))
+    (when (intersection '(:up :w) *key-bag*)
       (incf (y (pos *player*)) move-speed))
-    (when (or (member :down *key-bag*) (member :s *key-bag*))
+    (when (intersection '(:down :s) *key-bag*)
       (decf (y (pos *player*)) move-speed))
     (contain-player)))
 
 (defun bind-movement-button (button)
   (gamekit:bind-button button :pressed
-		       (lambda ()
-			 (push button *key-bag*)))
+                       (lambda ()
+                         (push button *key-bag*)))
   (gamekit:bind-button button :released
-		       (lambda ()
-			 (deletef *key-bag* button))))
+                       (lambda ()
+                         (deletef *key-bag* button))))
 
 (defmethod gamekit:post-initialize ((app example))
-  (loop for key in '(:z :y :w :s :a :d :left :right :up :down)
-     do (bind-movement-button key))
+  (loop :for key :in '(:z :y :w :s :a :d :left :right :up :down)
+        :do (bind-movement-button key))
   (reset-game))
 
 (defun direction-to-player (x y)
   (atan (- (y (pos *player*)) y)
-	(- (x (pos *player*)) x)))
+        (- (x (pos *player*)) x)))
 
 (defmethod move ((obj obj))
   (with-slots (pos heading) obj
@@ -128,19 +120,21 @@
 
 (defmethod draw ((obj obj))
   (gamekit:draw-circle (pos obj)
-		       (radius obj)
-		       :fill-paint (color obj)
-		       :stroke-paint *black*
-		       :fill-paint (vec4 0 0 0 0)
-		       :thickness 2))
+                       (radius obj)
+                       :fill-paint (color obj)
+                       :stroke-paint *black*
+                       :fill-paint (vec4 0 0 0 0)
+                       :thickness 2))
 
 (defun draw-shot (obj)
-  (gamekit:draw-circle (pos obj)
-		       (radius obj)
-		       :fill-paint (color obj)
-		       :stroke-paint (if (> (hp obj) 0) (color obj) *black*)
-		       :fill-paint (vec4 0 0 0 0)
-		       :thickness 2))
+  (with-accessors ((pos pos) (radius radius) (color color) (hp hp))
+      obj
+    (gamekit:draw-circle pos
+                         radius
+                         :fill-paint color
+                         :stroke-paint (if (> hp 0) color *black*)
+                         :fill-paint (vec4 0 0 0 0)
+                         :thickness 2)))
 
 
 (defun circles-collide-p (x y radius x2 y2 radius2)
@@ -155,137 +149,133 @@
 
 (defun player-collide-bullet ()
   (let ((collided nil))
-    (loop for s across *enemy-shots* do
-	 (when (objects-collide-p *player* s)
-	   (setf collided t))
-	 (when (and (circles-collide-p (x (pos s))
-				       (y (pos s))
-				       (radius s)
-				       (x (pos *player*))
-				       (y (pos *player*))
-				       *player-field-radius*)
-		    (> (hp s) 0))
-	   (incf *score* 300)
-	   (draw-lightning)
-	   (tickf *scrape-ticker*)
-	   (when (> *score* *high-score*) (setf *high-score* *score*))
-	   (setf (hp s) 0)))
+    (do-vector (s *enemy-shots*)
+      (when (objects-collide-p *player* s)
+        (setf collided t))
+      (when (and (circles-collide-p (x (pos s))
+                                    (y (pos s))
+                                    (radius s)
+                                    (x (pos *player*))
+                                    (y (pos *player*))
+                                    *player-field-radius*)
+                 (> (hp s) 0))
+        (incf *score* 300)
+        (draw-lightning)
+        (tickf *scrape-ticker*)
+        (when (> *score* *high-score*)
+          (setf *high-score* *score*))
+        (setf (hp s) 0)))
     (when collided
       (decf *lives*)
       (setf *enemy-shots* (make-game-container))
       (when (< *lives* 0)
-	(reset-game)))))
+        (reset-game)))))
 
 (defun enemy-shoot (x y speed direction color)
   (vector-push-extend
    (make-instance 'obj
-		  :pos (vec2 x y)
-		  :heading (vec2 (* (cos direction) speed)
-				 (* (sin direction) speed))
-		  :color color)
+                  :pos (vec2 x y)
+                  :heading (vec2 (* (cos direction) speed)
+                                 (* (sin direction) speed))
+                  :color color)
    *enemy-shots*))
 
 (defun shot-adapter-function (&key x y num-shots direction speed spread (color *BLACK*))
   (when (not (numberp num-shots))
     (return-from shot-adapter-function))
   (do-burst ((x-pos x)
-	     (y-pos y)
-	     (n num-shots)
-	     (dir direction)
-	     (spd speed)
-	     spread)
+             (y-pos y)
+             (n num-shots)
+             (dir direction)
+             (spd speed)
+             spread)
     (enemy-shoot x-pos y-pos spd dir color)))
 
 (defun draw-hud ()
   (draw-text (format nil "Highscore:~a" *high-score*) (vec2 0.0 (- *height*  18)))
-  (draw-text (format nil "Score:~a" *score*) (vec2 0.0 (- *height*  36)))
-  (draw-text (format nil "Lives:~a" *lives*) (vec2 0.0 (- *height*  54)))
-  (draw-text (format nil "Level:~a" *level*) (vec2 0.0 (- *height*  72)))
+  (draw-text (format nil "Score:~a" *score*) (vec2 0.0 (- *height* 36)))
+  (draw-text (format nil "Lives:~a" *lives*) (vec2 0.0 (- *height* 54)))
+  (draw-text (format nil "Level:~a" *level*) (vec2 0.0 (- *height* 72)))
   (draw-text (format nil "Percent:~a" (round
-				       (* 100.0
-					  (percent-done *scrape-ticker*))))
-	     (vec2 0.0 (- *height* 90)))
-  (loop for i across *enemies* do
-       (let* ((max-width 100.0)
-	      (width (* max-width (- 1 (percent-done *scrape-ticker*))))
-	      (half-width (* width .5))
-	      (y-offset 32)
-	      (pos (vec2 (parent-x i)
-			 (parent-y i))))
-	 (draw-line (add pos (vec2 (- half-width) y-offset))
-		    (add pos (vec2 half-width y-offset))
-		    *RED*
-		    :thickness 6))))
+                                       (* 100.0
+                                          (percent-done *scrape-ticker*))))
+             (vec2 0.0 (- *height* 90)))
+  (do-vector (i *enemies*)
+    (let* ((max-width 100.0)
+           (width (* max-width (- 1 (percent-done *scrape-ticker*))))
+           (half-width (* width .5))
+           (y-offset 32)
+           (pos (vec2 (parent-x i)
+                      (parent-y i))))
+      (draw-line (add pos (vec2 (- half-width) y-offset))
+                 (add pos (vec2 half-width y-offset))
+                 *RED*
+                 :thickness 6))))
 
 (defun lightning-point ()
-  (mult  (normalize
-	  (vec2 (* (expt -1.0 (random 2)) (random 1.0))
-		(* (expt -1.0 (random 2)) (random 1.0))))
-	 (float *player-field-radius*)))
+  (mult (normalize (vec2 (* (expt -1.0 (random 2)) (random 1.0))
+                         (* (expt -1.0 (random 2)) (random 1.0))))
+        (float *player-field-radius*)))
 
 (defun draw-lightning (&optional (color *BLACK*))
-  (loop for i across *enemies* do
-       (draw-curve (pos *player*)
-		   (vec2 (parent-x i)
-			 (parent-y i))
-		   (subt (pos *player*)
-			 (lightning-point))
-		   (subt (pos *player*)
-			 (lightning-point))
-		   color
-		   :thickness 4)))
+  (do-vector (i *enemies*)
+    (draw-curve (pos *player*)
+                (vec2 (parent-x i) (parent-y i))
+                (subt (pos *player*) (lightning-point))
+                (subt (pos *player*) (lightning-point))
+                color
+                :thickness 4)))
 
 (defun draw-player ()
   (draw-circle (pos *player*)
-	       *player-field-radius*
-	       :fill-paint (vec4 0 0 0 0)
-	       :stroke-paint *BLACK*
-	       :thickness 2)
+               *player-field-radius*
+               :fill-paint (vec4 0 0 0 0)
+               :stroke-paint *BLACK*
+               :thickness 2)
   (draw *player*))
 
 (defun draw-emitters ()
-  (loop for i across *enemies* do
-       (draw-circle (vec2 (parent-x i)
-			  (parent-y i))
-		    24
-		    :thickness 3
-		    :fill-paint *WHITE*
-		    :stroke-paint *RED*)))
+  (do-vector (i *enemies*)
+    (draw-circle (vec2 (parent-x i)
+                       (parent-y i))
+                 24
+                 :thickness 3
+                 :fill-paint *WHITE*
+                 :stroke-paint *RED*)))
 
 (defmethod gamekit:act ((this example))
   (update-pos))
 
 (defmethod gamekit:draw ((this example))
   (cond ((readyp *boss-warning-ticker*)
-	 (loop for i across *enemies* do (stepf i))
-	 (loop for i across *enemy-shots* do (move i))
+         (do-vector (i *enemies*) (stepf i))
+         (do-vector (i *enemy-shots*) (move i))
+         (do-vector (i *enemy-shots*) (draw-shot i))
+         (draw-emitters)
+         (player-collide-bullet)
+         (draw-player)
 
-	 (loop for i across *enemy-shots* do (draw-shot i))
-	 (draw-emitters)
-	 (player-collide-bullet)
-	 (draw-player)
+         (do-vector (i *enemy-shots*)
+           (with-slots (pos radius dead) i
+             (setf dead
+                   (or (> (- (x pos) radius) *width*)
+                       (> (- (y pos) radius) *height*)
+                       (< (+ (x pos) radius) 0.0)
+                       (< (+ (y pos) radius) 0.0)))))
 
-	 (loop for i across *enemy-shots* do
-	      (with-slots (pos radius dead) i
-		(setf dead
-		      (or (> (- (x pos) radius) *width*)
-			  (> (- (y pos) radius) *height*)
-			  (< (+ (x pos) radius) 0.0)
-			  (< (+ (y pos) radius) 0.0)))))
+         (setf *enemy-shots* (delete-if (lambda (a) (dead a)) *enemy-shots*))
+         (advance-level-if-done))
+        (t
+         (dotimes (i 4)
+           (draw-text "W A R N I N G !!!"
+                      (vec2 (* *width* .35)
+                            (- (* *height* .75) (* i 24)))
+                      :fill-color (nth i `(,*BLACK*
+                                           ,*RED*
+                                           ,*ORANGE*
+                                           ,*YELLOW*))))
 
-	 (setf *enemy-shots* (delete-if (lambda (a) (dead a)) *enemy-shots*))
-	 (advance-level-if-done))
-	(t
-	 (dotimes (i 4)
-	   (draw-text "W A R N I N G !!!"
-		      (vec2 (* *width* .35)
-			    (- (* *height* .75) (* i 24)))
-		      :fill-color (nth i `(,*BLACK*
-					   ,*RED*
-					   ,*ORANGE*
-					   ,*YELLOW*))))
-
-	 (tickf *boss-warning-ticker*)))
+         (tickf *boss-warning-ticker*)))
   (draw-hud))
 
 (defun run ()
